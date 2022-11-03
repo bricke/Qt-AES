43619ede59ce8692cb2900bc4f01780d51995539
pH-Valiu <110403905+pH-Valiu@users.noreply.github.com>
2022-11-03 16:23:13 +0100

    Removing prior old preprocessing code
    Co-authored-by: Matt <matteo.brichese@gmail.com>

--- aesni/aesni-enc-cbc.h
+++ aesni/aesni-enc-cbc.h
@@ -34,7 +34,6 @@ void AES_CBC_encrypt(const unsigned char *in,
     }
 }
 
-#if 1
 void AES_CBC_decrypt(const unsigned char *in,
                      unsigned char *out,
                      unsigned char ivec[16],
